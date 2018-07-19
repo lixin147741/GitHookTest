4f7b0fa454411a5accc3dcd9d55492cae8e8b624
lixin <lixin78@meituan.com>
2018-07-19 11:15:52 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 李鑫 on 2018/7/19.
+//  Copyright © 2018年 李鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by 李鑫 on 2018/7/19.
+//  Copyright © 2018年 李鑫. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
