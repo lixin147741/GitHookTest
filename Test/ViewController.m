7f8a007667b538ecf05c322e084c146dcbe94670
lixin <lixin78@meituan.com>
2018-10-12 15:55:00 +0800

    123

---
+++ Test/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by 李鑫 on 2018/10/12.
+//  Copyright © 2018 李鑫. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by 李鑫 on 2018/10/12.
+//  Copyright © 2018 李鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
